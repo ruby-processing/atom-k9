5f7ba39cec74e1586e29f9cbced24cc81398d3e3
monkstone <mamba2928@yahoo.co.uk>
2016-09-09 17:15:04 +0100

    a dummy spec should definetly pass

--- spec/atom-k9-spec.js
+++ spec/atom-k9-spec.js
@@ -1,8 +1,10 @@
 'use babel';
 
-import JRubyArt from '../lib/atom-k9';
-
-describe "when a test is written", ->
-  it "has some expectations that should pass", ->
-    expect("apples").toEqual("apples")
-    expect("oranges").not.toEqual("apples")
+describe('when a test is written', () => {
+  it('has some expectations that should pass', () => {
+    let apples = 'apples'
+    let oranges = 'oranges'
+    expect(apples).toBe('apples')
+    expect(oranges).toBe('oranges')
+  });
+});
